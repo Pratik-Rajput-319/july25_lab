297190ad33c46967666d9e3944aa82444ab0170a
Pratik-Rajput-319 <pratikrajput12@gmail.com>
2025-07-30 11:23:11 +0530

    My personal commit

--- suraaj.py
+++ suraaj.py
@@ -7,3 +7,5 @@ name = input("What is your name? ")
 print("Hello, " + name + "! Nice to meet you, July 25 Batch.")
 
 print("ADD TO CART!")
+
+print("Hello, Pratik was here to send you a Pull request") 
